e8204313486a07dcf5509cece21265cbff5d5f4e
Adithya J (AJ) <adithyajay@gmail.com>
2017-10-07 12:37:16 -0700

    pushing test file and the bing updated version

--- ml/bingme.py
+++ ml/bingme.py
@@ -1,6 +1,8 @@
 import scrt
 import json
 import urllib.request
+import urllib.parse
+import timeit
 
 
 """
@@ -10,10 +12,17 @@ Output: JSON file (to be the image)
 
 
 """
-def bingMe(query):
+def bingMe(query,callnum):
 	k,v = scrt.getMSFTKey()
+
+	args = urllib.parse.urlencode({'q': query.replace(' ','+'), k: v, 'mkt': 'en-us'})
+
 	query = query.replace(' ','+')
-	submission = 'https://api.cognitive.microsoft.com/bing/v5.0/images/search?q={0}&{1}={2}&mkt=en-us'.format(query,k,v)
-	response = urllib.request.urlopen(submission)
-	#data = json.load(response)
-	print(response.read())
+	submission = 'https://api.cognitive.microsoft.com/bing/v5.0/images/search?{0}'.format(args)
+	#submission = 'https://api.cognitive.microsoft.com/bing/v5.0/images/search?q={0}&{1}={2}&mkt=en-us'.format(query,k,v)
+	response = urllib.request.urlopen(submission).read().decode('utf-8')
+	#data = json.load(response) ##NEED STR NOT BYTE
+	print(response)
+	fin = open('{0}-{1}.json'.format(callnum,query),'w+')
+	fin.write(response)
+	print('Completed.')
