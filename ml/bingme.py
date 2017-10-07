0bd79a3e73efeccfee47e6225ab602a8d2979d5d
Adithya J (AJ) <adithyajay@gmail.com>
2017-10-07 12:58:47 -0700

    final bingme (atm) with no more test json (will need to cache actual data)

--- ml/bingme.py
+++ ml/bingme.py
@@ -2,27 +2,37 @@ import scrt
 import json
 import urllib.request
 import urllib.parse
-import timeit
+import time
 
 
 """
-bingME!
-Input: query generated
+bingME
+Input: query generated, call # (for json filename)
 Output: JSON file (to be the image)
 
 
 """
 def bingMe(query,callnum):
+	start = time.time()
 	k,v = scrt.getMSFTKey()
 
 	args = urllib.parse.urlencode({'q': query.replace(' ','+'), k: v, 'mkt': 'en-us'})
 
-	query = query.replace(' ','+')
-	submission = 'https://api.cognitive.microsoft.com/bing/v5.0/images/search?{0}'.format(args)
+	#backup
 	#submission = 'https://api.cognitive.microsoft.com/bing/v5.0/images/search?q={0}&{1}={2}&mkt=en-us'.format(query,k,v)
+	
+	submission = 'https://api.cognitive.microsoft.com/bing/v5.0/images/search?{0}'.format(args)
 	response = urllib.request.urlopen(submission).read().decode('utf-8')
-	#data = json.load(response) ##NEED STR NOT BYTE
-	print(response)
+	#data = json.loads(response)
+	
+	#print(json.dumps(data))
+
+	#writing to file, this erases the file if it already existed
+	fin = open('{0}-{1}.json'.format(callnum,query),'w+')
+	fin.close()
+
 	fin = open('{0}-{1}.json'.format(callnum,query),'w+')
 	fin.write(response)
-	print('Completed.')
+	end = time.time()
+	fin.close()
+	print('Completed in {0}.'.format(end-start))
