180adb7acf3c1104d711baf87a104298300adf3f
Adithya J (AJ) <adithyajay@gmail.com>
2017-10-08 06:31:25 -0700

    ml fix

--- ml/ml.py
+++ ml/ml.py
@@ -7,42 +7,38 @@ from bingme import *
 
 guiDict = {}
 count = 0
-MAX_CYCLE = 5
-def ml_main():
+query1 = 'landscape'
+query2 = 'city'
+def ml_main(param=None):
     global count
     global guiDict
-    query1 = 'landscape'
-    query2 = 'city'
-    while (count < MAX_CYCLE):
-        reply1,q1,c1=bingMe(query1,count)
-        reply2,q2,c2=bingMe(query2,count)
-        opt1 = imageURL(reply1)
-        opt2 = imageURL(reply2)
-        dler(opt1,q1,c1)
-        dler(opt2,q2,c2)
-        ans1 = imageUpload(q1,c1)
-        ans2 = imageUpload(q2,c2)
+    global query2
+    global query1
 
-        ##give shivam and yara the urls
-        left = paratu(ans1)
-        ml_left = ML(ans1)
-        right = paratu(ans2)
-        ml_right = ML(ans2)
-        ### end names
-        guiDict = {'first' : (left,ml_left), 'second': (right,ml_right)}
+    if param == 0:
+        query2 = ml_left[0]
+        print(query2,'\n')
+    elif param == 1:
+        query1 = ml_right[0]
+        print(query1,'\n')
+
+    reply1,q1,c1=bingMe(query1,count)
+    reply2,q2,c2=bingMe(query2,count)
+    opt1 = imageURL(reply1)
+    opt2 = imageURL(reply2)
+    dler(opt1,q1,c1)
+    dler(opt2,q2,c2)
+    ans1 = imageUpload(q1,c1)
+    ans2 = imageUpload(q2,c2)
+
+    ##give shivam and yara the urls
+    left = paratu(ans1)
+    ml_left = ML(ans1)
+    right = paratu(ans2)
+    ml_right = ML(ans2)
+    ### end names
+    guiDict = {'first' : (left,ml_left), 'second': (right,ml_right)}
         #print(guiDict,'\n')
-        if get_answer() == 0:
-            query2 = ml_left[0]
-            print(query2,'\n')
-        else:
-            query1 = ml_right[0]
-            print(query1,'\n')
-        count+=1 
-
-
-
-    print('end of loop, we must now give them a place')
-
 
     """
     reply,q,c=bingMe('landscape',0)
@@ -51,4 +47,3 @@ def ml_main():
     ans = imageUpload(q,c)
     print(ML(ans))
     """
-ml_main()
