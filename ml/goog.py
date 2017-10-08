80b1997bc3b068efa5a2fd050d368b711b75ebe6
Adithya J (AJ) <adithyajay@gmail.com>
2017-10-07 19:33:27 -0700

    env is literally the bain of my existance

--- ml/goog.py
+++ ml/goog.py
@@ -10,15 +10,13 @@ from google.cloud import vision
 
 def ML(picname,filename):
 
-	storage_client = storage.Client()
-	bucket = storage_client.get_bucket(os.environ.get('CLOUD_STORAGE_BUCKET'))
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(os.environ.get('CLOUD_STORAGE_BUCKET'))
     
 
-	vision_client = vision.ImageAnnotatorClient()
-	source_uri = 'gs://{}/{}'.format(os.environ.get('CLOUD_STORAGE_BUCKET'), filename)
-	response = vision_client.annotate_image({
-        'image': {'source': {'image_uri': source_uri}},
-    })
+    vision_client = vision.ImageAnnotatorClient()
+    source_uri = 'gs://{}/{}'.format(os.environ.get('CLOUD_STORAGE_BUCKET'), filename)
+    response = vision_client.annotate_image({'image': {'source': {'image_uri': source_uri}},})
     labels = response.label_annotations
     faces = response.face_annotations
     web_entities = response.web_detection.web_entities
